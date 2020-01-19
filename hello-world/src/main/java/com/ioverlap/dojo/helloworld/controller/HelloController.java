0a2b44c831c600f0451527a443fe4b2b8eae4c45
jnyupanqui <jnyupanqui@hotmail.com>
2020-01-19 11:24:11 -0800

    modify to restcontroller

--- hello-world/src/main/java/com/ioverlap/dojo/helloworld/controller/HelloController.java
+++ hello-world/src/main/java/com/ioverlap/dojo/helloworld/controller/HelloController.java
@@ -1,18 +1,15 @@
 package com.ioverlap.dojo.helloworld.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    @ResponseBody
+    @GetMapping(value = "/hello")
     public Map<String, String> hello(){
         Map<String, String> result = new HashMap<>();
         result.put("greeting", "Hello");
