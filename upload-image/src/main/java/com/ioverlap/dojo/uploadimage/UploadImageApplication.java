2688895cfc5f8dc852729bdc2d6d18537e59cc32
jnyupanqui <jnyupanqui@hotmail.com>
2020-01-19 16:22:20 -0800

    starter code

---
+++ upload-image/src/main/java/com/ioverlap/dojo/uploadimage/UploadImageApplication.java
@@ -0,0 +1,13 @@
+package com.ioverlap.dojo.uploadimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadImageApplication.class, args);
+	}
+
+}
