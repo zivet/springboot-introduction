7861d2fbf53427d47016f215a1fb6a43e620de1d
jnyupanqui <jnyupanqui@hotmail.com>
2020-01-27 13:22:49 -0800

    loop

--- thymeleaf/src/main/java/com/ioverlap/dojo/thymeleaf/controller/UserController.java
+++ thymeleaf/src/main/java/com/ioverlap/dojo/thymeleaf/controller/UserController.java
@@ -1,20 +1,25 @@
 package com.ioverlap.dojo.thymeleaf.controller;
 
+import com.ioverlap.dojo.thymeleaf.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.jws.WebParam;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
 
-    public static final String USER = "/demo";
+    public static final String DEMO = "/demo";
 
     public static final String GRADE = "/grade";
 
-    @RequestMapping(value = USER, method = RequestMethod.GET)
+    public static final String USER = "/user";
+
+    @RequestMapping(value = DEMO, method = RequestMethod.GET)
     public String demo(Model model) {
         model.addAttribute("hello", "Hello Thymeleaf");
         return "demo";
@@ -46,5 +51,14 @@ public class UserController {
         }
     }
 
+    @RequestMapping(value = USER, method = RequestMethod.GET)
+    public String user(Model model) {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User(1, "Tom", 30));
+        userList.add(new User(2, "Jerry", 29));
+        userList.add(new User(3, "Nancy", 27));
+        model.addAttribute("userList", userList);
+        return "user";
+    }
 
 }
