14cbb0d6989d8d01663faee05838d15985ca83cd
jnyupanqui <jnyupanqui@hotmail.com>
2020-01-27 13:13:30 -0800

    condition

--- thymeleaf/src/main/java/com/ioverlap/dojo/thymeleaf/controller/UserController.java
+++ thymeleaf/src/main/java/com/ioverlap/dojo/thymeleaf/controller/UserController.java
@@ -5,14 +5,46 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.jws.WebParam;
+
 @Controller
 public class UserController {
 
     public static final String USER = "/demo";
+
+    public static final String GRADE = "/grade";
 
     @RequestMapping(value = USER, method = RequestMethod.GET)
     public String demo(Model model) {
         model.addAttribute("hello", "Hello Thymeleaf");
         return "demo";
     }
+
+    @RequestMapping(value = GRADE, method = RequestMethod.GET)
+    public String grade(Model model) {
+        double grade = 90.5;
+        model.addAttribute("grade", grade);
+        model.addAttribute("gpa", convertGPA(grade));
+        return "grade";
+    }
+
+    private String convertGPA(double grade) {
+        if(grade >= 90) {
+            return "A";
+        }
+        else if(grade >=80) {
+            return "B";
+        }
+        else if(grade >= 70) {
+            return "C";
+        }
+        else if(grade >= 60) {
+            return "D";
+        }
+        else {
+            return "F";
+        }
+    }
+
+
 }
