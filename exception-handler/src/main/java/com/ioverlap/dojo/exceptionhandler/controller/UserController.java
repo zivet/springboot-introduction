a37efdb69f6211c253ca500b1aa8bad20b2930f6
jnyupanqui <jnyupanqui@hotmail.com>
2020-01-28 13:28:15 -0800

    handler into controller, for one specific exception

--- exception-handler/src/main/java/com/ioverlap/dojo/exceptionhandler/controller/UserController.java
+++ exception-handler/src/main/java/com/ioverlap/dojo/exceptionhandler/controller/UserController.java
@@ -1,15 +1,44 @@
 package com.ioverlap.dojo.exceptionhandler.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add() {
-        int num = 10 / 0;
-        return "add";
+        int num = 10 / 0;// throw exception
+        return "add";// not used
+    }
+
+    @ExceptionHandler(value = {
+            java.lang.ArithmeticException.class
+    })
+    public ModelAndView handlerArithmeticException(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e.toString());
+        modelAndView.setViewName("mathError");
+        return modelAndView;
+    }
+
+    @RequestMapping("/update")
+    public String update() {
+        String name = null;
+        name = name.toLowerCase();
+        return "update";
+    }
+
+    @ExceptionHandler(value = {
+            java.lang.NullPointerException.class
+    })
+    public ModelAndView handlerNullPointerException(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e.toString());
+        modelAndView.setViewName("nullPointerError");
+        return modelAndView;
     }
 }
