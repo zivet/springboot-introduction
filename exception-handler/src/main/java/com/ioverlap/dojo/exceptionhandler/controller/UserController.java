b6173326d39904ec1aa3aa10bbcb528a89a03c7b
jnyupanqui <jnyupanqui@hotmail.com>
2020-01-28 13:37:39 -0800

    all exception handlers centralized into one class

--- exception-handler/src/main/java/com/ioverlap/dojo/exceptionhandler/controller/UserController.java
+++ exception-handler/src/main/java/com/ioverlap/dojo/exceptionhandler/controller/UserController.java
@@ -1,10 +1,8 @@
 package com.ioverlap.dojo.exceptionhandler.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -15,30 +13,10 @@ public class UserController {
         return "add";// not used
     }
 
-    @ExceptionHandler(value = {
-            java.lang.ArithmeticException.class
-    })
-    public ModelAndView handlerArithmeticException(Exception e) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", e.toString());
-        modelAndView.setViewName("mathError");
-        return modelAndView;
-    }
-
     @RequestMapping("/update")
     public String update() {
         String name = null;
         name = name.toLowerCase();
         return "update";
-    }
-
-    @ExceptionHandler(value = {
-            java.lang.NullPointerException.class
-    })
-    public ModelAndView handlerNullPointerException(Exception e) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", e.toString());
-        modelAndView.setViewName("nullPointerError");
-        return modelAndView;
     }
 }
