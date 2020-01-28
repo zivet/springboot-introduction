d192d05be16ecc3d2cc657c9658ec7a584517c54
jnyupanqui <jnyupanqui@hotmail.com>
2020-01-28 09:08:14 -0800

    codebase

---
+++ validate-form/src/main/java/com/ioverlap/dojo/validateform/ValidateFormApplication.java
@@ -0,0 +1,13 @@
+package com.ioverlap.dojo.validateform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidateFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidateFormApplication.class, args);
+	}
+
+}
