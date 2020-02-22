a3a6c8fb8d5b9f62ff58eb38e9c2c44edeaf984e
jnyupanqui <jnyupanqui@hotmail.com>
2020-02-21 20:05:20 -0800

    add demo consuming soap-based webservice

---
+++ consuming-soap-webservice/src/test/java/com/ioverlap/dojo/consumingsoapwebservice/ConsumingSoapWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioverlap.dojo.consumingsoapwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingSoapWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
